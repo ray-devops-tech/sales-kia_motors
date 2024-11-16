d867cf7c728ffc0ee5b4e8a021ba86e5538213ee
anilradar <anilradar099@gmail.com>
2024-11-16 22:37:30 +0530

    abc.py file

--- abc.py
+++ abc.py
@@ -4,3 +4,11 @@ this is the second change in main abc
 
 
 this is the third change in main abc to play with pull command
+<<<<<<< Updated upstream
+=======
+
+
+echo "this is the line added for git stash testing"
+
+echo "final line for git stash practice"
+>>>>>>> Stashed changes
