1b172b269cd389b5a839ccd7872398d1ec552c90
anilradar <anilradar099@gmail.com>
2024-11-17 16:15:01 +0530

    add subnet in vpc.tf file

--- vpc.tf
+++ vpc.tf
@@ -9,3 +9,19 @@ resource "aws_vpc" "a-vpc" {
 }
 }
 
+resource "aws_subnet" "pub-sn-1" {
+   vpc_id = aws_vpc.a-vpc.id
+   cidr_block = "10.0.1.0/24"
+   avaialbility_zone = "us-east-1a"
+   map_public_ip_on_launch = true
+
+   tags = {
+      Name = "dev-public-sn-1"
+
+   }
+
+
+
+
+
+} 
